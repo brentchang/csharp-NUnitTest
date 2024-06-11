88bd804bc9ce2f6764600c5445d820ab0b07454e
Smars Hu <hubin.smars@gmail.com>
2024-06-11 14:35:25 -0400

    Unit Test Case 10 - 18

--- QAAssignment2.nUnitTests/ProductTest.cs
+++ QAAssignment2.nUnitTests/ProductTest.cs
@@ -71,5 +71,67 @@ namespace QAAssignment2.nUnitTests
             // Assert
             Assert.That(_product.Stock, Is.EqualTo(value));
         }
+
+        // test for ProductName property
+        // 3 Test cases for the same attribute
+        // empty string
+        [TestCase("")]
+        // normal string
+        [TestCase("YUKON WTML SPECIAL")]
+        // string of letter, number and special character
+        [TestCase("010-XIW2035$48_!143widj")]
+        public void ProductName_Test(string value)
+        {
+            // Assign
+            // the value of this test case is already assigned in the ProductName_Test method argument
+
+            // Act
+            _product.ProductName = value;
+
+            // Assert
+            Assert.That(_product.ProductName, Is.EqualTo(value));
+        }
+
+        // test for IncreaseStock method
+        // 3 Test cases for the same method
+        // Increase 0
+        [TestCase(0)]
+        // Increase just above 0
+        [TestCase(1)]
+        // Increase to maximum (100000)
+        [TestCase(99990)]
+        public void IncreaseStock_Test(int value)
+        {
+            // Assign
+            // the value of this test case is already assigned in the IncreaseStock_Test method argument
+
+            // Act
+            int orgStock = _product.Stock;
+            _product.IncreaseStock(value);
+
+            // Assert
+            Assert.That(_product.Stock, Is.EqualTo(orgStock + value));
+        }
+
+        // test for DecreaseStock method
+        // 3 Test cases for the same method
+        // Decrease 0
+        [TestCase(0)]
+        // Decrease just above 0
+        [TestCase(1)]
+        // Decrease to minimum (0)
+        [TestCase(10)]
+        public void DecreaseStock_Test(int value)
+        {
+            // Assign
+            // the value of this test case is already assigned in the DecreaseStock_Test method argument
+
+            // Act
+            int orgStock = _product.Stock;
+            _product.DecreaseStock(value);
+
+            // Assert
+            Assert.That(_product.Stock, Is.EqualTo(orgStock - value));
+        }
     }
 }
