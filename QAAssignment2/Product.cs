53a15607256dae386d2b3fc6a1dfff0b7cff779e
Brent Chang <45442719+brentchang@users.noreply.github.com>
2024-06-10 21:38:37 -0400

    Product class created

---
+++ QAAssignment2/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAAssignment2
+{
+    internal class Product
+    {
+        // properties
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+        // constructor
+        public Product(int productId, string productName, int price, int stock)
+        {
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        // methods
+        // method to increase stock
+        public void IncreaseStock(int number)
+        {
+            Stock += number;
+        }
+
+        // method to decrease stock
+        public void DecreaseStock(int number)
+        {
+            Stock -= number;
+        }
+    }
+}
