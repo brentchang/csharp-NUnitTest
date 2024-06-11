42d1d117b4a81d62f47656019f8b2fb83c156e24
Brent Chang <45442719+brentchang@users.noreply.github.com>
2024-06-10 22:23:02 -0400

    Added 9 test cases

--- QAAssignment2.nUnitTests/ProductTest.cs
+++ QAAssignment2.nUnitTests/ProductTest.cs
@@ -12,7 +12,7 @@ namespace QAAssignment2.nUnitTests
             _product = new Product(1, "Product1", 100, 10);
         }
 
-        // test for Product ID
+        // test for Product ID property
         // 3 Test cases for the same attribute
         // minimum value
         [TestCase(1)]
@@ -30,6 +30,46 @@ namespace QAAssignment2.nUnitTests
 
             // Assert
             Assert.That(_product.ProductID, Is.EqualTo(value));
+        }
+
+        // test for Price property
+        // 3 Test cases for the same attribute
+        // minimum value
+        [TestCase(1)]
+        // just above minimum value
+        [TestCase(2)]
+        // maximum value
+        [TestCase(5000)]
+        public void Price_Test(int value)
+        {
+            // Assign
+            // the value of this test case is already assigned in the Price_Test method argument
+
+            // Act
+            _product.Price = value;
+
+            // Assert
+            Assert.That(_product.Price, Is.EqualTo(value));
+        }
+
+        // test for Stock property
+        // 3 Test cases for the same attribute
+        // minimum value
+        [TestCase(1)]
+        // just above minimum value
+        [TestCase(2)]
+        // maximum value
+        [TestCase(100000)]
+        public void Stock_Test(int value)
+        {
+            // Assign
+            // the value of this test case is already assigned in the Stock_Test method argument
+
+            // Act
+            _product.Stock = value;
+
+            // Assert
+            Assert.That(_product.Stock, Is.EqualTo(value));
         }
     }
 }
--- QAAssignment2.nUnitTests/obj/Debug/net7.0/QAAssignment2.nUnitTests.AssemblyInfo.cs
+++ QAAssignment2.nUnitTests/obj/Debug/net7.0/QAAssignment2.nUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QAAssignment2.nUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b40b6b9b9f12a31b0ac1533a7848c41121dfb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1356273c4f0c47b4a27a5ff48a1908707a2b3968")]
 [assembly: System.Reflection.AssemblyProductAttribute("QAAssignment2.nUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QAAssignment2.nUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
